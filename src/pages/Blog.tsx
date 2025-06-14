d0489d98cdfc51dff7b56abc00c5de230b1ff533
changmen1 <zxl17339811909@163.com>
2025-06-14 16:27:02 +0800

    feat:header

---
+++ src/pages/Blog.tsx
@@ -0,0 +1,9 @@
+import type { FC } from "react";
+
+const Blog: FC = () => {
+    return (
+        <div>博客</div>
+    )
+}
+
+export default Blog;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import type { FC } from "react";
+
+const Home: FC = () => {
+    return (
+        <div>主页</div>
+    )
+}
+
+export default Home;
