6290efc9edbc947ee5337783d29d7302b7338c40
changmen1 <zxl17339811909@163.com>
2025-06-17 08:47:49 +0800

    feat:添加像素风

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,17 +1,21 @@
 import type { FC } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { AnimatePresence } from "motion/react";
 
 const DefaultLayout: FC = () => {
+    const location = useLocation();
     return (
         <div className="relative z-10 flex w-full flex-col bg-white">
             <SidebarProvider>
                 <SidebarInset>
                     <Header />
                     <main className="flex-1 bg-main mt-[100px]">
-                        <Outlet />
+                        <AnimatePresence mode="wait">
+                            <Outlet key={location.pathname} />
+                        </AnimatePresence>
                     </main>
                     <Footer />
                 </SidebarInset>
