ffd27788916912c2b3f847faed4111f95841e42d
changmen1 <zxl17339811909@163.com>
2025-06-19 00:32:03 +0800

    feat:添加顶部动画

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,8 +1,15 @@
+import Aurora from "@/components/bits/Aurora";
 import type { FC } from "react";
 
 const Blog: FC = () => {
     return (
-        <div className="pt-10">
+        <div className="pt-5">
+            <Aurora
+                colorStops={["#3A29FF", "#FF94B4", "#FF3232"]}
+                blend={0.5}
+                amplitude={1.0}
+                speed={0.5}
+            />
             这里是小主的自传编写 还在努力编写中~~~~~~~~~~~~~~~~~~~~~~~~
         </div>
     )
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,15 +1,23 @@
+import Aurora from "@/components/bits/Aurora";
 import Error from "@/components/bits/Error";
 import type { FC } from "react";
 
-
 const NotFound: FC = () => {
     return (
-        <div className="flex items-center justify-center h-screen bg-black">
-            <Error
-                baseIntensity={0.2}
-            >
-                404啦请联系小主
-            </Error>
+        <div className="pt-5">
+            <Aurora
+                colorStops={["#3A29FF", "#FF94B4", "#FF3232"]}
+                blend={0.5}
+                amplitude={1.0}
+                speed={0.5}
+            />
+            <div className="flex items-center justify-center">
+                <Error
+                    baseIntensity={0.2}
+                >
+                    404啦请联系小主
+                </Error>
+            </div>
         </div>
     )
 }
