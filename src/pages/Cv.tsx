21ded4d536703401379f7b7b7eceabbd913d8a80
changmen1 <zxl17339811909@163.com>
2025-06-19 00:32:03 +0800

    feat:添加顶部动画

--- src/pages/Cv.tsx
+++ src/pages/Cv.tsx
@@ -1,10 +1,19 @@
+import Aurora from "@/components/bits/Aurora";
 import type { FC } from "react";
 
 const Cv: FC = () => {
     return (
-        <div className="pt-10">
-            这里是小主的简历 还在努力编写中~~~~~~~~~~~~~~~~~~~~~~~~
-        </div>
+        <>
+            <div className="pt-5">
+                <Aurora
+                    colorStops={["#3A29FF", "#FF94B4", "#FF3232"]}
+                    blend={0.5}
+                    amplitude={1.0}
+                    speed={0.5}
+                />
+                这里是小主的简历 还在努力编写中~~~~~~~~~~~~~~~~~~~~~~~~
+            </div>
+        </>
     )
 }
 
