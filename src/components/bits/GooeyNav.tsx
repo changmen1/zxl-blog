62d1593ef127a227c39a5b40659cee0c73bab2e0
changmen1 <zxl17339811909@163.com>
2025-06-23 23:43:18 +0800

    fix:hdaderBUG修复

--- src/components/bits/GooeyNav.tsx
+++ src/components/bits/GooeyNav.tsx
@@ -320,7 +320,7 @@ const GooeyNav: React.FC<GooeyNavProps> = ({
                         {items.map((item, index) => (
                             <li
                                 key={index}
-                                className={`text-[13px] py-[0.6em] px-[1em] rounded-full relative cursor-pointer transition-[background-color_color_box-shadow] duration-300 ease shadow-[0_0_0.5px_1.5px_transparent] text-white ${activeIndex === index ? "active" : ""
+                                className={`py-[0.6em] px-[1em] rounded-full relative cursor-pointer transition-[background-color_color_box-shadow] duration-300 ease shadow-[0_0_0.5px_1.5px_transparent] text-white ${activeIndex === index ? "active" : ""
                                     }`}
                                 onClick={(e) => handleClick(e, index, item.href)}
                             >
